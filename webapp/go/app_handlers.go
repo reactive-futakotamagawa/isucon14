a9a3512eeb84bf8dd51ea47b568f0c8cea9790c9
pikachu0310 <pikachu13711@gmail.com>
2024-12-08 14:06:17 +0900

    Revert 遅くするRetryを
    This reverts commit 090ed9a060d3080a33027d3f64f1faa03052c771.

--- webapp/go/app_handlers.go
+++ webapp/go/app_handlers.go
@@ -665,7 +665,7 @@ func (h *apiHandler) appGetNotification(w http.ResponseWriter, r *http.Request) 
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE user_id = ? ORDER BY created_at DESC LIMIT 1`, user.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &appGetNotificationResponse{
-				RetryAfterMs: 100,
+				RetryAfterMs: 30,
 			})
 			return
 		}
@@ -712,7 +712,7 @@ func (h *apiHandler) appGetNotification(w http.ResponseWriter, r *http.Request) 
 			CreatedAt: ride.CreatedAt.UnixMilli(),
 			UpdateAt:  ride.UpdatedAt.UnixMilli(),
 		},
-		RetryAfterMs: 100,
+		RetryAfterMs: 30,
 	}
 
 	if ride.ChairID.Valid {
--- webapp/go/chair_handlers.go
+++ webapp/go/chair_handlers.go
@@ -201,7 +201,7 @@ func (h *apiHandler) chairGetNotification(w http.ResponseWriter, r *http.Request
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id = ? ORDER BY updated_at DESC LIMIT 1`, chair.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &chairGetNotificationResponse{
-				RetryAfterMs: 100,
+				RetryAfterMs: 30,
 			})
 			return
 		}
@@ -261,7 +261,7 @@ func (h *apiHandler) chairGetNotification(w http.ResponseWriter, r *http.Request
 			},
 			Status: status,
 		},
-		RetryAfterMs: 100,
+		RetryAfterMs: 30,
 	})
 }
 
