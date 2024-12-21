54b7b989310e27da34fb8036f77cf2ede870ecd2
H1rono <hronok66@gmail.com>
2024-12-21 22:15:17 +0900

    Add `FOREIGN KEY`

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -103,7 +103,8 @@ CREATE TABLE ride_statuses
   created_at      DATETIME(6)                                                                NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '状態変更日時',
   app_sent_at     DATETIME(6)                                                                NULL COMMENT 'ユーザーへの状態通知日時',
   chair_sent_at   DATETIME(6)                                                                NULL COMMENT '椅子への状態通知日時',
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (ride_id) REFERENCES rides (id) ON UPDATE CASCADE ON DELETE CASCADE
 )
   COMMENT = 'ライドステータスの変更履歴テーブル';
 
